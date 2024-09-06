34767e8ce0211f1b2db6fb9632a42d4484e6583b
PierreRicardo213 <pierre182013@gmail.com>
2024-09-06 12:01:12 -0300

    dados.js

--- dados.js
+++ dados.js
@@ -1,39 +1,44 @@
 let dados = [
     {
         titulo: "Milton Nascimento",
-        descricao: "Milton Nascimento é um dos maiores nomes da música brasileira, reconhecido mundialmente por sua voz marcante e suas composições atemporais. Nascido em 26 de outubro de 1942, emTijuca, Rio de Janeiro, Milton, aos 81 anos, continua sendo uma referência na MPB. Suas músicas, que misturam elementos da música popular brasileira com influências internacionais, marcaram gerações.Entre as suas canções mais conhecidas, destacam- se Maria, Maria, um clássico que fala sobre a beleza e a força da mulher brasileira, Canção da América, uma reflexão sobre a identidade nacional e a busca por um mundo mais justo, e Travessia, uma canção que fala sobre a jornada da vida e a busca por significado.",
-        link : "https://pt.wikipedia.org/wiki/Milton_Nascimento#Biografia",
-        instagram : "https://www.instagram.com/miltonbitucanascimento/",
-        spotify : "https://open.spotify.com/embed/artist/3Bnq7jiU506HcPjRgQ43TM?utm_source=generator"
-    
+        descricao: "Milton Nascimento é um dos maiores nomes da música brasileira, reconhecido mundialmente por sua voz marcante e suas composições atemporais.Entre as suas canções mais conhecidas, destacam- se Maria, Maria, um clássico que fala sobre a beleza e a força da mulher brasileira, Canção da América, uma reflexão sobre a identidade nacional e a busca por um mundo mais justo, e Travessia, uma canção que fala sobre a jornada da vida e a busca por significado.",
+        link: "https://pt.wikipedia.org/wiki/Milton_Nascimento#Biografia",
+        instagram: "https://www.instagram.com/miltonbitucanascimento/",
+        spotify: "https://open.spotify.com/embed/artist/3Bnq7jiU506HcPjRgQ43TM?utm_source=generator",
+        tags: "Musico mineiro minas gerais violao cantor compositor samba bossa nova musica"
+
     },
     {
         titulo: "Lô Borges",
         descricao: "Lô Borges é um dos principais nomes do Clube da Esquina, movimento musical que revolucionou a MPB. Conhecido por sua voz suave e melodias envolventes, suas composições são marcadas por uma profunda conexão com a natureza e a cultura mineira. Entre suas músicas mais conhecidas, destacam-se 'Para Lennon', 'Vento Ventania' e 'Nada Será Como Antes'.",
         link: "https://en.wikipedia.org/wiki/L%C3%B4_Borges",
         instagram: "https://www.instagram.com/loborgesoficial/",
-        spotify: "https://open.spotify.com/embed/artist/1mZbRiSVb2ExneAlDbBiiT?utm_source=generator"
+        spotify: "https://open.spotify.com/embed/artist/1mZbRiSVb2ExneAlDbBiiT?utm_source=generator",
+        tags: "Musico mineiro minas gerais violao cantor compositor samba bossa nova musica"
     },
     {
         titulo: "João Bosco",
         descricao: "João Bosco é um cantor, compositor e instrumentista brasileiro, conhecido por suas letras inteligentes e melodias sofisticadas. Suas parcerias com Aldir Blanc são um marco da MPB, resultando em sucessos como 'O bêbado e a equilibrista'.",
         link: "https://en.wikipedia.org/wiki/Jo%C3%A3o_Bosco",
         instagram: "https://www.instagram.com/joaoboscoreal/reels/",
-        spotify: "https://open.spotify.com/embed/artist/3DF0ClNOUuvS3gh8V8sRJH?utm_source=generator"
+        spotify: "https://open.spotify.com/embed/artist/3DF0ClNOUuvS3gh8V8sRJH?utm_source=generator",
+        tags: "Musico mineiro minas gerais violao cantor compositor samba bossa nova musica"
     },
     {
         titulo: "Paulinho Pedra Azul",
         descricao: "Paulinho Pedra Azul é um cantor, compositor e instrumentista brasileiro, conhecido por sua voz marcante e letras poéticas. Sua música, que mescla elementos da MPB com influências internacionais, é marcada por uma grande sensibilidade e profundidade.",
         link: "https://en.wikipedia.org/wiki/Pedra_Azul",
         instagram: "https://www.instagram.com/paulinhopedraazul/?hl=en",
-        spotify: "https://open.spotify.com/embed/artist/4c0vWJnX4IViBT7JNp40iS?utm_source=generator"
+        spotify: "https://open.spotify.com/embed/artist/4c0vWJnX4IViBT7JNp40iS?utm_source=generator",
+        tags: "Musico mineiro minas gerais violao cantor compositor samba bossa nova musica"
     },
     {
         titulo: "Flávio Venturini",
         descricao: "Flávio Venturini é um dos principais nomes do Clube da Esquina. Seu trabalho como cantor, compositor e multi-instrumentista é marcado por uma grande versatilidade e uma profunda pesquisa musical.",
         link: "https://en.wikipedia.org/wiki/Fl%C3%A1vio_Venturini",
         instagram: "https://www.instagram.com/flavioventurinioficial/?hl=en",
-        spotify: "https://open.spotify.com/embed/artist/6y7OyrOw8yA4yq4airggji?utm_source=generator"
+        spotify: "https://open.spotify.com/embed/artist/6y7OyrOw8yA4yq4airggji?utm_source=generator",
+        tags: "Musico mineiro minas gerais violao cantor compositor samba bossa nova musica"
     }
 ];
 
